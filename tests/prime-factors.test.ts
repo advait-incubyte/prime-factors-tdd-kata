e0a6053be521c7c6a524b236c184595e56379b92
Advait Maindalkar <advait@incubyte.co>
2025-06-17 09:14:26 +0530

    failing test case for 2

--- tests/prime-factors.test.ts
+++ tests/prime-factors.test.ts
@@ -9,4 +9,8 @@ describe('Prime Factors', () => {
     it('should return an empty array for 1', () => {
         expect(primeFactors(1)).toEqual([]);
     })
+
+    it('should return [2] for 2', () => {
+        expect(primeFactors(2)).toEqual([2]);
+    })
 })
